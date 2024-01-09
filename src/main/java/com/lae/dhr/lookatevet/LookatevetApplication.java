7ae673a8afdc869c61c6c7a579ef7a2fe8b20194
diegohuapayarivera <diego.huapaya@vallegrande.edu.pe>
2024-01-08 21:56:28 -0500

    MOre archive ups

---
+++ src/main/java/com/lae/dhr/lookatevet/LookatevetApplication.java
@@ -0,0 +1,13 @@
+package com.lae.dhr.lookatevet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookatevetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookatevetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lae/dhr/lookatevet/LookatevetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lae.dhr.lookatevet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookatevetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
